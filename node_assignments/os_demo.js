0406ae39aa417d5ec892b90a8fc0a94724411a2c
mithildhure <mithil.dhure1021@gmail.com>
2026-01-27 13:48:43 +0530

    initial commit

---
+++ node_assignments/os_demo.js
@@ -0,0 +1,7 @@
+// import OS package/library/module
+const os = require("os");           //operating system
+
+console.log(os.freemem());    //Shows Free Memory
+console.log(os.homedir());    //location of home directory
+console.log(os.hostname());   //Host Name
+console.log(os.platform());   //OS type
