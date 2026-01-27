9dd6893f8c7d3a4a8c19719df3be36d728b11619
mithildhure <mithil.dhure1021@gmail.com>
2026-01-27 16:07:40 +0530

    Commit Succesfull

---
+++ node_assignments/fs_demo.js
@@ -0,0 +1,51 @@
+const fs = require("fs");
+
+// fs.writeFile("node_assignments/text1.txt","Hello",(err)=>{
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log("File Created");
+//     }
+// });
+
+// fs.readFile("node_assignments/text1.txt",(err,data)=>{
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log(data.toString());
+//     }
+// });
+
+// fs.appendFile("node_assignments/text1.txt","\nThis is new Content",(err)=>{
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log("Append Succefull");
+//     }
+// });
+
+// fs.unlink("node_assignments/text1.txt",(err)=>{
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log("File Deleted");
+//     }
+// });
+
+// ====================Folder==================================
+
+// fs.mkdir("demo1",(err)=>{
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log("Folder Created");
+//     }
+// });
+
+// fs.rmdir("./demo1",(err)=>{
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log("Folder Deleted");
+//     }
+// });
